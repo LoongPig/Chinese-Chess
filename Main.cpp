e18b3060c0ab6423a131bd310e3ec129ca3e4fd7
LoongPig <DragonPigLZY@163.com>
2025-09-27 13:41:18 +0800

    13:41:16

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,7 @@ int main(){
         RedStart:
         MessageBox(NULL,L"Red select move chess",L"Prompt",MB_OK);
         coord p1=_const_.selectChess();
+        if(_const_.gColor(p1)!=red) goto RedStart;
         MessageBox(NULL,L"Red select where to go",L"Prompt",MB_OK);
         coord p2=_const_.selectChess();
         if(!_const_.checkWay(p1,p2)) goto RedStart;
@@ -17,6 +18,7 @@ int main(){
         BlackStart:
         MessageBox(NULL,L"Black select move chess",L"Prompt",MB_OK);
         p1=_const_.selectChess();
+        if(_const_.gColor(p2)!=black) goto BlackStart;
         MessageBox(NULL,L"Black select where to go",L"Prompt",MB_OK);
         p2=_const_.selectChess();
         if(!_const_.checkWay(p1,p2)) goto BlackStart;
